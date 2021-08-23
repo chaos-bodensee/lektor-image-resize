e9d4222fcd5162dda2145b7e0b0809cda62c7a84
L3D <l3d@c3woc.de>
2021-08-23 22:14:38 +0200

    create lektor_image-resize

---
+++ lektor_image_resize.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+import os
+import shutil
+
+from lektor.build_programs import AttachmentBuildProgram, buildprogram
+from lektor.context import get_ctx
+from lektor.db import Image
+from lektor.imagetools import (compute_dimensions, find_imagemagick,
+                               get_image_info, get_quality)
+from lektor.pluginsystem import Plugin
+from lektor.reporter import reporter
+from lektor.utils import portable_popen, locate_executable
+from werkzeug.utils import cached_property
+
+# We override process_image here because Lektor does not support adding extra
+# parameters yet, but maybe it will soon, and this can be removed when it does.
+def process_image(
+    ctx,
+    source_image,
+    dst_filename,
+    width=None,
+    height=None,
+    mode=None,
+    quality=None,
+    extra_params=None,
+):
+    """Build image from source image, optionally compressing and resizing.
+    "source_image" is the absolute path of the source in the content directory,
+    "dst_filename" is the absolute path of the target in the output directory.
+    """
+    if width is None and height is None:
+        raise ValueError("Must specify at least one of width or height.")
+
+    guetzli = locate_executable('guetzli')
+
+    im = find_imagemagick(ctx.build_state.config["IMAGEMAGICK_EXECUTABLE"])
+
+    if quality is None:
+        quality = get_quality(source_image)
+
+    resize_key = ""
+    if width is not None:
+        resize_key += str(width)
+    if height is not None:
+        resize_key += "x" + str(height)
+
+    cmdline = [im, source_image, "-auto-orient"]
+    cmd2line = [guetzli]
+    cmdline += ["-resize", resize_key]
+
+    if extra_params:
+        cmdline.extend(extra_params)
+
+    cmdline += ["-quality", str(quality), dst_filename]
+    cmd2line += ["--quality", str(quality), dst_filename, dst_filename]
+
+    reporter.report_debug_info("imagemagick cmd line", cmdline)
+    portable_popen(cmdline).wait()
+    reporter.report_debug_info("guetzli cmd line", cmd2line)
+    portable_popen(cmd2line).wait()
+
+@buildprogram(Image)
+class ResizedImageBuildProgram(AttachmentBuildProgram):
+    def build_artifact(self, artifact):
+        ctx = get_ctx()
+        plugin = ctx.env.plugins["image-resize"]
+        config = plugin.config
+
+        artifact.ensure_dir()
+        AttachmentBuildProgram.build_artifact(self, artifact)
+
+        if not config:
+            return
+
+        source_img = artifact.source_obj.attachment_filename
+
+        with open(source_img, "rb") as f:
+            _, w, h = get_image_info(f)
+
+        # For every section in the config, we need to generate one image.
+        for item, conf in config.items():
+            width = int(conf["max_width"])
+            height = int(conf.get("max_height", "0"))
+
+            if not height:
+                _, height = compute_dimensions(width, None, w, h)
+
+            df = artifact.source_obj.url_path
+            ext_pos = df.rfind(".")
+            if df[ext_pos + 1 :] == 'svg':
+                dst_filename = "%s-%s.png" % (df[:ext_pos], item)
+            else:
+                dst_filename = "%s-%s.%s" % (df[:ext_pos], item, df[ext_pos + 1 :])
+
+            def closure(dst_filename, source_img, width, height, resize_image=True):
+                # We need this closure, otherwise variables get updated and this
+                # doesn't work at all.
+                @ctx.sub_artifact(artifact_name=dst_filename, sources=[source_img])
+                def build_thumbnail_artifact(artifact):
+                    artifact.ensure_dir()
+                    if not resize_image:
+                        shutil.copy2(source_img, artifact.dst_filename)
+                    else:
+                        process_image(
+                            ctx,
+                            source_img,
+                            artifact.dst_filename,
+                            width,
+                            height,
+                            quality=85,
+                            extra_params=[
+                                "-strip",
+                                "-interlace",
+                                "Plane",
+                            ],
+                        )
+
+            # If the image is larger than the max_width, resize it, otherwise
+            # just copy it.
+            resize_image = w > width or h > height
+            closure(dst_filename, source_img, width, height, resize_image)
+
+
+class ImageResizePlugin(Plugin):
+    name = u"thumbnail-generator"
+    description = u"A configurable way to generate thumbnails and optimize them with guetzli."
+    image_exts = ["jpg", "jpeg"]
+
+    @cached_property
+    def config(self):
+        conf = self.get_config()
+        return {section: conf.section_as_dict(section) for section in conf.sections()}
---
+++ setup.py
@@ -0,0 +1,38 @@
+import ast
+import io
+import re
+
+from setuptools import setup, find_packages
+
+with io.open('README.md', 'rt', encoding="utf8") as f:
+    readme = f.read()
+
+_description_re = re.compile(r'description\s+=\s+(?P<description>.*)')
+
+with open('lektor_image_resize.py', 'rb') as f:
+    description = str(ast.literal_eval(_description_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+setup(
+    author='L3D',
+    author_email='l3d@c3woc.de',
+    description=description,
+    keywords='Lektor plugin image resize',
+    license='MIT',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    name='lektor-image-resize',
+    packages=find_packages(),
+    py_modules=['lektor_image_resize'],
+    url='https://github.com/chaos-bodensee/lektor-image-resize.git',
+    version='0.1',
+    classifiers=[
+        'Framework :: Lektor',
+        'Environment :: Plugins',
+    ],
+    entry_points={
+        'lektor.plugins': [
+            'image-resize = lektor_image_resize:ImageResizePlugin',
+        ]
+    }
+)
